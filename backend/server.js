a9c7a9ae9b4c2ec077dc388f0b708f3d411ba098
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-02-07 15:29:08 +0530

    changed 17 files

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,22 @@
 const express = require("express");
-
+const notes = require("./data/notes");
 const app = express();
-app.listen(5000, console.log("server is running 5000 port"));
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server is running ${PORT} port`));
+
+app.get("/", (req, res) => {
+  res.json("API is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((c) => c._id === req.params.id);
+
+  res.send(note);
+});
