989b420ebdbe88af1d89e70c1852e9444bf5af2a
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-02-16 21:38:32 +0530

    modified userController.js and userModel.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,5 @@
-// userController.js
-
-// Example user data for demonstration purposes
-const users = [
-  { id: 1, name: "John Doe", email: "john@example.com" },
-  { id: 2, name: "Jane Smith", email: "jane@example.com" },
-];
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
 // Get all users
 const getAllUsers = (req, res) => {
@@ -24,7 +19,7 @@ const getUserById = (req, res) => {
 };
 
 // Create a new user
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   // In a real application, you would validate the request body before creating the user
   const { name, email, password, pic } = req.body;
   const newUser = { name, email, password, pic };
@@ -35,7 +30,7 @@ const registerUser = (req, res) => {
   const responseUser = { name, email };
 
   res.status(201).json(responseUser);
-};
+});
 
 // Update user by ID
 const updateUserById = (req, res) => {
