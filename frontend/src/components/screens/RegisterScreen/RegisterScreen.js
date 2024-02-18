9fb2d1e35b23ca0fe17400ef85f06f8915fb2fc8
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-02-18 19:31:10 +0530

    modified RegisterScreen.js in RegisterScreen

--- frontend/src/components/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/components/screens/RegisterScreen/RegisterScreen.js
@@ -1,10 +1,97 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import React from "react";
+import { Link } from "react-router-dom";
+import MainScreen from "../../MainScreen";
+
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const RegisterScreen = () => {
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  });
+
   return (
-    <div>
-      <h1>Register Screen</h1>
-    </div>
+    <MainScreen title="REGISTER">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <form style={{ width: "100%", maxWidth: "400px" }}>
+          <TextField
+            id="name"
+            label="Name"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            sx={{ width: "100%" }} // Set width to 100%
+          />
+          <TextField
+            id="email"
+            label="Email Address"
+            type="email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            sx={{ width: "100%" }} // Set width to 100%
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            sx={{ width: "100%" }} // Set width to 100%
+          />
+          <TextField
+            id="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            sx={{ width: "100%" }} // Set width to 100%
+          />
+          <div style={{ marginTop: "15px" }}>
+            <Button
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon />}
+              type="image/png"
+            >
+              Upload profile picture
+              <input type="file" style={{ display: "none" }} />
+            </Button>
+          </div>
+
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            style={{ marginTop: "1rem" }}
+          >
+            Register
+          </Button>
+        </form>
+        <Typography variant="body1" style={{ marginTop: "1rem" }}>
+          Have an Account?{" "}
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            Login
+          </Link>
+        </Typography>
+      </Box>
+    </MainScreen>
   );
 };
 
